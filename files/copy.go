98fe683f3a67d95435642002a181cc4959ff7e81
David Gageot <david@gageot.net>
2017-04-18 17:03:55 +0200

    Support output to stdout for any action
    Signed-off-by: David Gageot <david@gageot.net>

--- files/copy.go
+++ files/copy.go
@@ -9,46 +9,33 @@ import (
 // Copy copies a file to a given destination. It makes sur parent folders are
 // created on the way. Use `-` to copy to stdout.
 func Copy(src, dst string) error {
-	if dst != "-" {
-		if err := MkdirAll(filepath.Dir(dst)); err != nil {
-			return err
-		}
-	}
-
 	in, err := os.Open(src)
 	if err != nil {
 		return err
 	}
 	defer in.Close()
 
-	var out io.Writer
-	if dst != "-" {
-		destOut, err := os.Create(dst)
-		if err != nil {
-			return err
-		}
-		defer destOut.Close()
-
-		out = destOut
-	} else {
-		out = os.Stdout
-	}
-
-	_, err = io.Copy(out, in)
-	return err
+	return CopyFrom(dst, 0666, in)
 }
 
 func CopyFrom(dst string, mode os.FileMode, reader io.Reader) error {
-	if err := MkdirAll(filepath.Dir(dst)); err != nil {
-		return err
+	var out io.Writer
+	if dst == "-" {
+		out = os.Stdout
+	} else {
+		if err := MkdirAll(filepath.Dir(dst)); err != nil {
+			return err
+		}
+
+		file, err := os.OpenFile(dst, os.O_CREATE|os.O_TRUNC|os.O_WRONLY, mode)
+		if err != nil {
+			return err
+		}
+		defer file.Close()
+
+		out = file
 	}
 
-	file, err := os.OpenFile(dst, os.O_CREATE|os.O_TRUNC|os.O_WRONLY, mode)
-	if err != nil {
-		return err
-	}
-	defer file.Close()
-
-	_, err = io.Copy(file, reader)
+	_, err := io.Copy(out, reader)
 	return err
 }
